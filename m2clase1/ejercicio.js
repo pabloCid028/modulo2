b6bb03331ac71c9a439edb84fda04d509a77777d
pablocid028 <pablo.cid028@gmail.com>
2022-05-30 12:14:32 -0400

    Modulo2

---
+++ m2clase1/ejercicio.js
@@ -0,0 +1,6 @@
+const nombre = prompt ('Buenas tardes, ¿cual es tu nombre?');
+let saludo = 'Hola' + nombre;
+const edad = prompt ('Dinos que edad tienes');
+saludo = 'Hola ' + nombre + " de " + edad + ' años';
+alert (saludo);
+console.log (saludo)
