e02e2125b4769553f064ad420ee95de783e3a052
pablocid028 <pablo.cid028@gmail.com>
2022-05-30 16:24:08 -0400

    m2clase1asincronico

---
+++ m2clase1-asincronico/ejercicio.js
@@ -0,0 +1,16 @@
+const edad = prompt ('¿ Qué edad tienes?');
+const diasedad = ( edad * 365);
+saludo = ('Felicidades!! \n'+ 'Ud ha vivido ' + diasedad + ' dias considerando sus ' + edad + ' años');
+alert (saludo);
+// parte 2
+const edadpablo = 34;
+const edadmaxima = 81;
+const tiempodevida = ( edadmaxima - edadpablo)
+const snickers = 2;
+const snickersporcomer = ((tiempodevida * 365 ) * snickers)
+const preciosnickers = 890
+const porgastarensnickers = (preciosnickers * snickersporcomer)
+porcomer = ('Necesitaras ' + snickersporcomer + ' snickers para que te alcancen hasta los ' + tiempodevida + ' años que aun te quedan por vivir')
+alert (porcomer)
+gastototal = ('El costo total de ' + snickersporcomer + ' snickers que te quedan por comer en tu vida es de ' + '$'+ porgastarensnickers)
+alert (gastototal)
